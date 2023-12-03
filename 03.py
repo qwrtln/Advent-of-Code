95a1b1fbc2cdf9b5e9ad2271ffdb21bd9811c768
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2023-12-03 12:22:56 +0100

    Add solution to day 3 part 2

--- 03.py
+++ 03.py
@@ -1,5 +1,7 @@
 import string
 
+from collections import defaultdict
+
 from common import get_puzzle
 
 
@@ -34,10 +36,17 @@ def find_number_neighbours(number, starting_point):
 
 def find_neighbouring_symbols(neighbours, puzzle):
     symbols = []
-    for (y, x) in neighbours:
+    for y, x in neighbours:
         if puzzle[y][x] != ".":
             symbols.append(puzzle[y][x])
     return symbols
+
+
+def find_neighbouring_gear(neighbours, puzzle):
+    gears = []
+    for y, x in neighbours:
+        if puzzle[y][x] == "*":
+            return (y, x)
 
 
 if __name__ == "__main__":
@@ -46,15 +55,25 @@ if __name__ == "__main__":
     puzzle = add_borders(puzzle)
     # print("\n".join(puzzle))
 
-    result = 0
+    result_1 = 0
+    result_2 = 0
+
+    gears_w_neighbours = defaultdict(list)
 
     for number, point in find_numbers(puzzle):
         neighbours = find_number_neighbours(number, point)
+
         symbols = find_neighbouring_symbols(neighbours, puzzle)
         if symbols:
-            # input(f"{number} has neighbouring {symbols=}, so adding it")
-            result += int(number)
-        # else:
-        #     input(f"{number} has no neighbouring symbols")
+            result_1 += int(number)
 
-    print(result)
+        gear = find_neighbouring_gear(neighbours, puzzle)
+        if gear:
+            gears_w_neighbours[gear].append(int(number))
+
+    for numbers in gears_w_neighbours.values():
+        if len(numbers) == 2:
+            result_2 += numbers[0] * numbers[1]
+
+    print(f"1: {result_1}")
+    print(f"2: {result_2}")

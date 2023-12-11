ad0d258309d2f77ab9c8876a0d6fa6dc0a2f8017
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2023-12-11 23:19:40 +0100

    Day 11 part 2

--- 11.py
+++ 11.py
@@ -3,36 +3,32 @@ import itertools
 
 puzzle = [l for l in open("inputs/11.txt").read().strip().split("\n")]
 
-
-expanded_rows = []
-for row in puzzle:
-    expanded_rows.append(row)
-    if set(row) == {"."}:
-        expanded_rows.append(row)
-        
-
-expanded_columns = []
-for x in range(len(expanded_rows[0])):
-    col = "".join(l[x] for l in expanded_rows)
-    expanded_columns.append(col)
-    if set(col) == {"."}:
-        expanded_columns.append(col)
+empty_rows_index = [i for i, row in enumerate(puzzle) if set(row) == {"."}]
+empty_cols_index = [
+    x for x in range(len(puzzle[0])) if set(l[x] for l in puzzle) == {"."}
+]
 
 
-expanded_puzzle = []
-for y in range(len(expanded_columns[0])):
-    expanded_puzzle.append("".join([l[y] for l in expanded_columns]))
-
-
-puzzle = expanded_puzzle
-galaxies = set()
-for y in range(len(puzzle)):
-    for x in range(len(puzzle[0])):
+def solve(puzzle, empty_factor):
+    galaxies = set()
+    for y, x in [(y, x) for y in range(len(puzzle)) for x in range(len(puzzle[0]))]:
         if puzzle[y][x] == "#":
             galaxies.add((y, x))
 
-result_1 = 0
-for (y1, x1), (y2, x2) in itertools.combinations(galaxies, 2):
-    result_1 += abs(y2 - y1) + abs(x2 - x1)
+    result = 0
+    for (y1, x1), (y2, x2) in itertools.combinations(galaxies, 2):
+        result += abs(x2 - x1)
+        for c in empty_cols_index:
+            if c in range(x1, x2, 1 if x1 < x2 else -1):
+                result += empty_factor - 1
 
-print(result_1)
+        result += abs(y2 - y1)
+        for r in empty_rows_index:
+            if r in range(y1, y2, 1 if y1 < y2 else -1):
+                result += empty_factor - 1
+
+    return result
+
+
+print("1:", solve(puzzle, empty_factor=2))
+print("2:", solve(puzzle, empty_factor=10**6))

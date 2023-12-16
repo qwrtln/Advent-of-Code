969e160134e644448409c0928ef761a292443928
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2023-12-16 23:08:22 +0100

    Day 14 pt 1

--- 14.py
+++ 14.py
@@ -1,4 +1,12 @@
-puzzle = [line for line in open("inputs/14-sample.txt").read().strip().split("\n")]
+puzzle = [line for line in open("inputs/14.txt").read().strip().split("\n")]
 
-for line in puzzle:
-    print(line)
+
+cols = ["".join([r[i] for r in puzzle]) for i in range(len(puzzle[0]))]
+result_1 = 0
+for col in cols:
+    sorted_col = "#".join(["".join(sorted(li, reverse=True)) for li in col.split("#")])
+    for index, row in enumerate(range(len(sorted_col), 0, -1)):
+        if sorted_col[index] == "O":
+            result_1 += row
+
+print("1:", result_1)

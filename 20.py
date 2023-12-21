6fb688aca6036d9b82f36f0bebf0acf3976cfa09
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2023-12-21 11:11:25 +0100

    Make day 20 part 2 cleaner

--- 20.py
+++ 20.py
@@ -4,15 +4,13 @@ import itertools
 import math
 import queue
 
-# import graphviz
+import graphviz
 
 
 puzzle = [line for line in open("inputs/20.txt").read().strip().split("\n")]
 
-target_mapping = {"lg": "rr", "gr": "js", "bn": "bs", "st": "zb"}
 
-
-class Pulse(str, enum.Enum):
+class Pulse(enum.StrEnum):
     LOW = "low"
     HIGH = "high"
 
@@ -28,9 +26,6 @@ class FlipFlop:
             return Pulse.HIGH if self.on else Pulse.LOW
 
 
-node = None
-
-
 @dataclasses.dataclass
 class Conjunction:
     name: str
@@ -39,15 +34,8 @@ class Conjunction:
     def receive(self, pulse: Pulse, source: str):
         self.inputs[source] = pulse
         if all([p == Pulse.HIGH for p in self.inputs.values()]):
-            if self.name in target_mapping:
-                global node
-                node = self.name
             return Pulse.LOW
         return Pulse.HIGH
-
-    @property
-    def watches(self):
-        return len(self.inputs)
 
 
 @dataclasses.dataclass
@@ -62,9 +50,7 @@ def create_circuit(puzzle):
     circuit = {}
     for line in puzzle:
         source, dest = line.split(" -> ")
-        if source[0] in "%&":
-            source = source[1:]
-        circuit[source] = dest.split(", ")
+        circuit[source.strip("%&")] = dest.split(", ")
     return circuit
 
 
@@ -86,77 +72,66 @@ def create_module_name_mapping(puzzle, circuit):
     return module_name_mapping
 
 
-candidates = ["js", "bs", "rr", "zb"]
-early_candidates = ["lg", "gr", "bn", "st"]
-starters = ["nd", "fx", "mc", "lf"]
-
-found = {t: 0 for t in target_mapping}
-
-
-def handle_button_click(circuit, module_name_mapping, times):
+def handle_button_clicks(circuit, module_name_mapping, times, critical_modules):
     pulses = {
         Pulse.LOW: 0,
         Pulse.HIGH: 0,
     }
+    cycles = {t: 0 for t in critical_modules}
     tasks = queue.Queue()
-    for i in itertools.count(start=1):
-        global node
-        if node:
-            print(node, i)
-            found[node] = i
-            node = None
-        if all(found.values()):
-            print([v for v in found.values()])
-            result = math.prod([v - 1 for v in found.values()])
-            print(result)
-            assert result in range(224981741117232, 226106087649552)
-            return
-        if i % 100000 == 0:
-            print(i)
+
+    for i in itertools.count():
+        if i == times:
+            # pulses sent after {times} button presses
+            yield pulses
         tasks.put(("broadcaster", Pulse.LOW))
         pulses[Pulse.LOW] += 1
+
         while not tasks.empty():
             source, pulse = tasks.get()
             targets = circuit[source]
-            if source in target_mapping and target == target_mapping[source]:
-                input(f"{source=} {target=} {i=} {pulse=}")
-            for target in targets:
-                pulses[pulse] += 1
-                if module := module_name_mapping.get(target, None):
-                    if new_pulse := module.receive(pulse, source):
-                        tasks.put((module.name, new_pulse))
 
-    return pulses
+            for target in targets:
+                # source is connected to target(s)
+                pulses[pulse] += 1
+                if target not in module_name_mapping:
+                    # target is not sending to any modules
+                    continue
+                module = module_name_mapping[target]
+                if new_pulse := module.receive(pulse, source):
+                    # target emits a pulse upon receiving from {source}
+                    if module.name in critical_modules and new_pulse == Pulse.LOW:
+                        # one of the critical mdoules emitted low pulse
+                        # it means all its neighbours have high state
+                        cycles[module.name] = i + 1
+                        values = [v for v in cycles.values()]
+                        if all(values):
+                            # all critical modules reached
+                            yield math.lcm(*values)
+                    # put the new pulse in the queue
+                    tasks.put((module.name, new_pulse))
 
 
 circuit = create_circuit(puzzle)
 module_name_mapping = create_module_name_mapping(puzzle, circuit)
-dummies = set()
-for targets in circuit.values():
-    for t in targets:
-        if t not in circuit:
-            dummies.add(t)
+critical_modules = ["lg", "gr", "bn", "st"]  # derived from the graph plotted below
+
+results = handle_button_clicks(circuit, module_name_mapping, 1000, critical_modules)
+print("1:", math.prod(next(results).values()))
+print("2:", next(results))
 
 
-pulses = handle_button_click(circuit, module_name_mapping, 1000)
+def plot(puzzle, strip_broadcaster=False):
+    graph = graphviz.Digraph()
+    for line in puzzle:
+        node_name, nodes = line.split(" -> ")
+        nodes = nodes.split(", ")
+        node_name = node_name.strip("&%")
+        if strip_broadcaster and node_name == "broadcaster":
+            continue
+        for node in nodes:
+            graph.edge(node_name, node)
+    print(graph.render(directory="visual").replace("\\", "/"))
 
-result_1 = pulses[Pulse.LOW] * pulses[Pulse.HIGH]
-print("1:", result_1)
 
-LOWW = 224981741117232
-HIGH = 226106087649552
-
-# def plot(puzzle, strip_broadcast=False):
-#     dot = graphviz.Digraph()
-#     for line in puzzle:
-#         node_name, _, nodes = line.partition(" -> ")
-#         nodes = nodes.split(", ")
-#         node_name = node_name.strip("&%")
-#         if strip_broadcast and node_name == "broadcaster":
-#             continue
-#         for node in nodes:
-#             dot.edge(node_name, node)
-#         # print(node_name, nodes)
-#     print(dot.render(directory="visual").replace("\\", "/"))
-#
 # plot(puzzle)

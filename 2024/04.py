8d55e84a5a9344571c781c6f266247fd00ba7647
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-05 10:41:01 +0100

    Simplify day 4

--- 2024/04.py
+++ 2024/04.py
@@ -19,27 +19,25 @@ def find_vertical(puzzle):
 
 def find_diagonal(puzzle):
     result = 0
-    # finding right diagonal
+    # right diagonal
     for y in range(0, len(puzzle) - len(WORD) + 1):
         for x in range(0, len(puzzle[0]) - len(WORD) + 1):
-            current = (
+            if (
                 puzzle[y][x]
                 + puzzle[y + 1][x + 1]
                 + puzzle[y + 2][x + 2]
                 + puzzle[y + 3][x + 3]
-            )
-            if current in (WORD, WORD[::-1]):
+            ) in (WORD, WORD[::-1]):
                 result += 1
-    # finding left diagonal
+    # left diagonal
     for y in range(len(puzzle) - 1, len(WORD) - 2, -1):
         for x in range(0, len(puzzle[0]) - len(WORD) + 1):
-            current = (
+            if (
                 puzzle[y][x]
                 + puzzle[y - 1][x + 1]
                 + puzzle[y - 2][x + 2]
                 + puzzle[y - 3][x + 3]
-            )
-            if current in (WORD, WORD[::-1]):
+            ) in (WORD, WORD[::-1]):
                 result += 1
     return result
 
@@ -48,13 +46,12 @@ def find_cross(puzzle):
     result = 0
     for y, line in enumerate(puzzle[1 : len(puzzle) - 1], start=1):
         for x, char in enumerate(line[1 : len(line) - 1], start=1):
-            if char == "A":
-                if (
-                    f"{puzzle[y + 1][x + 1]}A{puzzle[y - 1][x - 1]}" in ("SAM", "MAS")
-                ) and (
-                    f"{puzzle[y + 1][x - 1]}A{puzzle[y - 1][x + 1]}" in ("SAM", "MAS")
-                ):
-                    result += 1
+            if (
+                char == "A"
+                and f"{puzzle[y + 1][x + 1]}A{puzzle[y - 1][x - 1]}" in ("SAM", "MAS")
+                and f"{puzzle[y + 1][x - 1]}A{puzzle[y - 1][x + 1]}" in ("SAM", "MAS")
+            ):
+                result += 1
     return result
 
 

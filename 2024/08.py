6b4c12a34f1aa1af9f576b17361df6d269f13a1d
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-08 16:57:13 +0100

    Simplify day 8

--- 2024/08.py
+++ 2024/08.py
@@ -7,14 +7,13 @@ HEIGHT = len(puzzle)
 WIDTH = len(puzzle[0])
 
 
-ANTENNAS_MAPPING = collections.defaultdict(list)
-LOCATION_MAPPING = {}
-
-for y in range(len(puzzle)):
-    for x in range(len(puzzle[y])):
-        if puzzle[y][x] != ".":
-            ANTENNAS_MAPPING[puzzle[y][x]].append((y, x))
-            LOCATION_MAPPING[(y, x)] = puzzle[y][x]
+def parse_puzzle(puzzle):
+    antennas_mapping = collections.defaultdict(list)
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[y])):
+            if puzzle[y][x] != ".":
+                antennas_mapping[puzzle[y][x]].append((y, x))
+    return antennas_mapping
 
 
 def get_neighbouring_antinodes(p1, p2):
@@ -34,50 +33,44 @@ def get_neighbouring_antinodes(p1, p2):
         dx = p1[1] - p2[1]
         a_x_1 = p1[1] + dx
         a_x_2 = p2[1] - dx
-    return (a_y_1, a_x_1), (a_y_2, a_x_2)
+    return [
+        p
+        for p in [(a_y_1, a_x_1), (a_y_2, a_x_2)]
+        if p[0] in range(HEIGHT) and p[1] in range(WIDTH)
+    ]
 
 
-def get_all_antinode_positions(p1, p2):
+def get_all_antinodes(p1, p2):
     a_y_1, a_y_2, a_x_1, a_x_2 = [], [], [], []
     if p1[0] < p2[0]:
         dy = p2[0] - p1[0]
-        a_y_1 = list(range(p1[0], -1, -dy))
-        a_y_2 = list(range(p2[0], HEIGHT, dy))
+        a_y_1 = range(p1[0], -1, -dy)
+        a_y_2 = range(p2[0], HEIGHT, dy)
     else:
         dy = p1[0] - p2[0]
-        a_y_1 = list(range(p1[0], HEIGHT, dy))
-        a_y_2 = list(range(p2[0], -1, -dy))
+        a_y_1 = range(p1[0], HEIGHT, dy)
+        a_y_2 = range(p2[0], -1, -dy)
     if p1[1] < p2[1]:
         dx = p2[1] - p1[1]
-        a_x_1 = list(range(p1[1], -1, -dx))
-        a_x_2 = list(range(p2[1], WIDTH, dx))
+        a_x_1 = range(p1[1], -1, -dx)
+        a_x_2 = range(p2[1], WIDTH, dx)
     else:
         dx = p1[1] - p2[1]
-        a_x_1 = list(range(p1[1], WIDTH, dx))
-        a_x_2 = list(range(p2[1], -1, -dx))
-    for y, x in zip(a_y_1, a_x_1):
-        yield (y, x)
-    for y, x in zip(a_y_2, a_x_2):
-        yield (y, x)
-    yield p1
-    return p2
-
-
-def point_in_map(p):
-    if p[0] in range(HEIGHT) and p[1] in range(WIDTH):
-        return True
-    return False
+        a_x_1 = range(p1[1], WIDTH, dx)
+        a_x_2 = range(p2[1], -1, -dx)
+    yield from zip(a_y_1, a_x_1)
+    yield from zip(a_y_2, a_x_2)
+    return (p1, p2)
 
 
 def count_antinodes(mapping, neighbouring_func):
     anti_positions = set()
-    for node, positions in mapping.items():
+    for positions in mapping.values():
         for p1, p2 in itertools.combinations(positions, 2):
-            for node in neighbouring_func(p1, p2):
-                if point_in_map(node):
-                    anti_positions.add(node)
+            anti_positions.update(neighbouring_func(p1, p2))
     return len(anti_positions)
 
 
-print("1:", count_antinodes(ANTENNAS_MAPPING, get_neighbouring_antinodes))
-print("2:", count_antinodes(ANTENNAS_MAPPING, get_all_antinode_positions))
+antennas_mapping = parse_puzzle(puzzle)
+print("1:", count_antinodes(antennas_mapping, get_neighbouring_antinodes))
+print("2:", count_antinodes(antennas_mapping, get_all_antinodes))

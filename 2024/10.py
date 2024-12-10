69fd5c4c6b649b9bfa78d844983aaf061b052144
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-10 09:00:34 +0100

    Add both parts to day 10

--- 2024/10.py
+++ 2024/10.py
@@ -1,14 +1,18 @@
-import queue
+# Benchmark: CPython (3.12.7)
+#   Time (mean ± σ):      28.4 ms ±   2.1 ms    [User: 25.7 ms, System: 2.7 ms]
+#   Range (min … max):    25.5 ms …  35.8 ms    98 runs
+#
+# Benchmark: pypy (3.10.14)
+#   Time (mean ± σ):     129.0 ms ±  10.3 ms    [User: 113.1 ms, System: 15.2 ms]
+#   Range (min … max):   120.5 ms … 154.1 ms    21 runs
+#
+import collections
 
 puzzle = [list(line) for line in open("inputs/10.txt").read().strip().split("\n")]
 
 
 WIDTH = len(puzzle[0])
 HEIGHT = len(puzzle)
-
-
-# for line in puzzle:
-#     print("".join(line))
 
 
 def find_starting_points(puzzle):
@@ -24,32 +28,37 @@ def get_passable_neighbours(point):
         if (
             0 <= y + dy < HEIGHT
             and 0 <= x + dx < WIDTH
-            and puzzle[y + dy][x + dx] != "."
             and (int(puzzle[y + dy][x + dx]) - int(puzzle[y][x])) == 1
         ):
             yield y + dy, x + dx
 
 
-def find_path(point, puzzle, path=None, visited=None):
-    if path is None:
-        path = []
+def find_path_score(point, puzzle, visited=None):
     if visited is None:
         visited = set()
-    path.append(point)
     visited.add(point)
     y, x = point
     if puzzle[y][x] == "9":
-        yield path
+        yield 1
     for neighbour in get_passable_neighbours((y, x)):
         if neighbour not in visited:
-            yield from find_path(neighbour, puzzle, path, visited)
+            yield from find_path_score(neighbour, puzzle, visited)
 
 
-def calculate_trailhead_scores(puzzle):
-    result = 0
-    for s in find_starting_points(puzzle):
-        result += len(list(find_path(s, puzzle)))
-    return result
+def find_all_paths(start_point, puzzle):
+    queue = collections.deque()
+    seen = set([start_point])
+    queue.append([start_point])
+    while queue:
+        path = queue.popleft()
+        y, x = path[-1]
+        if puzzle[y][x] == "9":
+            yield 1  # or path[1:] for the actual path
+        for neighbour in get_passable_neighbours((y, x)):
+            queue.append(path + [neighbour])
+            seen.add(neighbour)
+    return seen
 
 
-print("1:", calculate_trailhead_scores(puzzle))
+print("1:", sum(sum(find_path_score(s, puzzle)) for s in find_starting_points(puzzle)))
+print("2:", sum(sum(find_all_paths(s, puzzle)) for s in find_starting_points(puzzle)))

ba85bce804afa3d8833b8fac3ba0ec8b12fe2a65
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-13 17:25:45 +0100

    Add benchmarks to day 12

--- 2024/12.py
+++ 2024/12.py
@@ -1,8 +1,15 @@
-# Sadly, pt2 does not work
+# Benchmark: CPython (3.12.7)
+#   Time (mean ± σ):     983.0 ms ±  25.5 ms    [User: 1511.0 ms, System: 14.6 ms]
+#   Range (min … max):   947.6 ms … 1024.5 ms    10 runs
+#
+# Benchmark: pypy (3.10.14-7.3.17)
+#   Time (mean ± σ):     14.889 s ±  0.259 s    [User: 15.332 s, System: 0.046 s]
+#   Range (min … max):   14.444 s … 15.317 s    10 runs
+#
 import numpy as np
 
 GARDEN = np.array(
-    [list(line) for line in open("inputs/12-test2.txt").read().strip().split("\n")]
+    [list(line) for line in open("inputs/12.txt").read().strip().split("\n")]
 )
 
 WIDTH, HEIGHT = GARDEN.shape
@@ -13,11 +20,6 @@ def get_plants(garden):
     for row in garden:
         plants.update(row)
     return plants
-
-
-for line in GARDEN:
-    print("".join(line))
-print("---------------")
 
 
 def get_neighbours(x, y):
@@ -54,290 +56,54 @@ def find_islands(x, y, plant, garden):
     return islands
 
 
-def count_point_corners(point, island, plant=None):
+def count_point_corners(point, island):
     x, y = point
     # fmt: off
-    UL, UC, UR = (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
-    CL, _,  CR = (x - 1, y    ), None,       (x + 1, y    )
-    DL, DC, DR = (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)
-    # fmt: on
-    up = [UL, UC, UR]
-    down = [DL, DC, DR]
-    left = [UL, CL, DL]
-    right = [UR, CR, DR]
-    diagonal = [UL, UR, DR, DL]
-    orthogonal = [UC, CR, DC, CL]
-    all_neighbours = [*orthogonal, *diagonal]
-    sum_of_neighbours = sum(n in island for n in all_neighbours)
-    if sum_of_neighbours == 0:
-        return 4
-    if sum_of_neighbours == 1:
-        return 2
-    if sum_of_neighbours == 2:
-        if all(n in island for n in [UC, DC]) or (all(n in island for n in [CL, CR])):
-            # e.g.
-            #
-            # ...
-            # EEE
-            # ...
-            return 0
-        if (
-            all(n in island for n in up)
-            or (all(n in island for n in down))
-            or all(n in island for n in left)
-            or (all(n in island for n in right))
-        ):
-            # e.g.
-            #
-            # .EE
-            # .E.
-            # ...
-            return 2
-        # e.g.
-        #
-        # .E.
-        # .E.
-        # E..
-        return 2
-    if sum_of_neighbours == 3:
-        if (
-            all(n in island for n in up)
-            or (all(n in island for n in down))
-            or (all(n in island for n in left))
-            or (all(n in island for n in right))
-        ):
-            # ...
-            # .E.
-            # EEE
-            return 2
-        elif sum(n in island for n in diagonal) == 2:
-            # E.E
-            # .EE
-            # ...
-            return 2
-        elif sum(n in island for n in diagonal) == 1 and (
-            (sum(n in island for n in up) == 0 and sum(n in island for n in right) == 0)
-            or (
-                sum(n in island for n in right) == 0
-                and sum(n in island for n in down) == 0
-            )
-            or (
-                sum(n in island for n in down) == 0
-                and sum(n in island for n in left) == 0
-            )
-            or (
-                sum(n in island for n in left) == 0
-                and sum(n in island for n in up) == 0
-            )
-        ):
-            # ...
-            # .EE
-            # .EE
-            return 1
-        elif sum(n in island for n in diagonal) == 1 and (
-            sum(n in island for n in (UC, DC)) == 2
-            or sum(n in island for n in (CL, CR)) == 2
-        ):
-            # .E.
-            # .E.
-            # .EE
-            return 0
-        return 2
-        # input("Always 2?")
-    if sum_of_neighbours == 4:
-        if all(n in island for n in orthogonal):
-            # .E.
-            # EEE
-            # .E.
-            return 4
-        elif sum(n in island for n in diagonal) == 2 and (
-            (all(n in island for n in up) and DC in island)
-            or (all(n in island for n in down) and UC in island)
-            or (all(n in island for n in left) and CR in island)
-            or (all(n in island for n in right) and CL in island)
-        ):
-            # E..
-            # EEE
-            # E..
-            return 0
-        elif all(n in island for n in [UC, UL, UR, DR]) or (
-            all(n in island for n in [UC, UR, CR, DL])
-            or all(n in island for n in [CR, DR, DC, UL])
-            or all(n in island for n in [DC, DL, CL, UR])
-        ):
-            # EE.
-            # EE.
-            # ..E
-            return 1
-        elif sum(n in island for n in diagonal) == 2 and (
-            sum(n in island for n in (CL, CR)) == 2
-            or sum(n in island for n in (UC, DC)) == 2
-        ):
-            # .EE
-            # .E.
-            # .EE
-            return 0
-        elif sum(n in island for n in diagonal) == 1 and (
-            (sum(n in island for n in up) == 1 and sum(n in island for n in down) == 1)
-            or (
-                sum(n in island for n in right) == 1
-                and sum(n in island for n in left) == 1
-            )
-        ):
-            # ..E
-            # EEE
-            # .E.
-            return 2
-        elif sum(n in island for n in diagonal) == 2 and (
-            (sum(n in island for n in left) == 2 and sum(n in island for n in up) == 2)
-            or (
-                sum(n in island for n in up) == 2
-                and sum(n in island for n in right) == 2
-            )
-            or (
-                sum(n in island for n in right) == 2
-                and sum(n in island for n in down) == 2
-            )
-            or (
-                sum(n in island for n in down) == 2
-                and sum(n in island for n in left) == 2
-            )
-        ):
-            # .EE
-            # EE.
-            # E..
-            return 2
-        else:
-            # .EE
-            # EE.
-            # EE.
-            return 1
-        # elif all(n in island for n in down):
-        # EEE
-        # EEE
-        # .E.
-        # return 2
-    if sum_of_neighbours == 5:
-        if (
-            (all(n in island for n in up) and CL in island and CR in island)
-            or (all(n in island for n in down) and CL in island and CR in island)
-            or (all(n in island for n in left) and UC in island and DC in island)
-            or (all(n in island for n in right) and UC in island and DC in island)
-        ):
-            # EE.
-            # EE.
-            # EE.
-            return 0
-        elif (
-            (
-                all(n in island for n in up)
-                and CL not in island
-                and CR not in island
-                and DC in island
-            )
-            or (
-                all(n in island for n in down)
-                and CL not in island
-                and CR not in island
-                and UC in island
-            )
-            or (
-                all(n in island for n in left)
-                and UC not in island
-                and DC not in island
-                and CR in island
-            )
-            or (
-                all(n in island for n in right)
-                and UC not in island
-                and DC not in island
-                and CL in island
-            )
-        ):
-            # EEE
-            # .E.
-            # .EE
-            return 0
-        elif all(n in island for n in diagonal) and (
-            all(n in island for n in up)
-            or all(n in island for n in down)
-            or all(n in island for n in left)
-            or all(n in island for n in right)
-        ):
-            # EEE
-            # .E.
-            # E.E
-            return 2
-        else:
-            # E.E
-            # .EE
-            # .EE
-            return 1
-    if sum_of_neighbours == 6:
-        if sum(n in island for n in diagonal) == 4:
-            # E.E
-            # EEE
-            # E.E
-            return 0
-        elif sum(n in island for n in orthogonal) == 4:
-            # .E.
-            # EEE
-            # EEE
-            return 2
-        elif sum(n in island for n in diagonal) == 3 and (
-            (all(n in island for n in up) and all(n in island for n in right))
-            or (all(n in island for n in right) and all(n in island for n in down))
-            or (all(n in island for n in down) and all(n in island for n in left))
-            or (all(n in island for n in left) and all(n in island for n in up))
-        ):
-            # EE.
-            # EE.
-            # EEE
-            return 0
-        elif sum(n in island for n in diagonal) == 3 and (
-            (all(n in island for n in right) and sum(n in island for n in left) == 2)
-            or (all(n in island for n in left) and sum(n in island for n in right) == 2)
-            or (all(n in island for n in up) and sum(n in island for n in down) == 2)
-            or (all(n in island for n in down) and sum(n in island for n in up) == 2)
-        ):
-            # .EE
-            # EEE
-            # E.E
-            return 1
-        # else:
-        #     # everything else
-        #     #
-        #     # E..
-        #     # EEE
-        #     # EEE
-        #     return 0
-        input("Unknown")
-    if sum_of_neighbours == 7:
-        non_neighbour = [n for n in all_neighbours if n not in island][0]
-        if non_neighbour in orthogonal:
-            # E.E
-            # EEE <- rotated four times
-            # EEE
-            return 2
-        else:
-            # EE.
-            # EEE <- rotated four times
-            # EEE
-            return 1
+    matrices = [
+            [(-1, -1), ( 0, -1),  # 12.
+             (-1,  0),         ], # 3x.
+            #-----------------    # ...
 
-    print("Well well")
-    # plot_garden(all_neighbours, plant)
-    return 0
+            [( 0, -1), ( 1, -1),  # .12
+                       ( 1,  0)], # .x3
+            #-----------------    # ...
+
+            [          ( 1,  0),  # ...
+             ( 0,  1), ( 1,  1)], # .x1
+            #-----------------    # .23
+
+            [(-1,  0),            # ...
+             (-1,  1), ( 0,  1)], # 1x.
+            #-----------------    # 23.
+    ]
+    convex = [[0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 0],
+              [1, 0, 1],
+              ]
+    concave = [[1, 0, 0],
+               [0, 1, 0],
+               [0, 0, 1],
+               [0, 1, 0],
+              ]
+    # fmt: on
+    corners = 0
+    for index, mask in enumerate(matrices):
+        neighbours = [(int(x + dx), int(y + dy)) for dx, dy in mask]
+        in_island = [int(p in island) for p in neighbours]
+        if sum(in_island) == 0:
+            corners += 1
+        if in_island == concave[index]:
+            corners += 1
+        if in_island == convex[index]:
+            corners += 1
+    return corners
 
 
 def calculate_perimiter(island, plant):
     perimiter = 0
     boundary = set()
     for x, y in island:
-        # For a special case
-        UL, UC, UR = (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
-        CL, _, CR = (x - 1, y), None, (x + 1, y)
-        DL, DC, DR = (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)
         dd = ((-1, 0), (1, 0), (0, -1), (0, 1))
         for dx, dy in dd:
             x_n = x + dx
@@ -349,6 +115,14 @@ def calculate_perimiter(island, plant):
             ):
                 boundary.add((x, y))
                 perimiter += 1
+        # fmt: off
+        UL, UC, UR = (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+        CL, _,  CR = (x - 1, y),      None,      (x + 1, y    )
+        DL, DC, DR = (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)
+        # fmt: on
+        # .E.
+        # EEE <- points for a special case
+        # .E.
         if (
             0 <= x + 1 < WIDTH
             and 0 <= x - 1 < WIDTH
@@ -361,23 +135,8 @@ def calculate_perimiter(island, plant):
     return perimiter, boundary
 
 
-def plot_garden(point, plant):
-    plot = ""
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            if (y, x) == point:
-                plot += "."
-            elif GARDEN[y][x] == plant:
-                plot += GARDEN[y][x]
-            else:
-                plot += " "
-        plot += "\n"
-    print(plot)
-
-
 result_1 = 0
 result_2 = 0
-message = []
 for plant in get_plants(GARDEN):
     x, y = np.where(GARDEN == plant)
     area = np.array([x, y]).T
@@ -386,24 +145,12 @@ for plant in get_plants(GARDEN):
         area = np.array(island).size // 2
         perimiter, boundary_points = calculate_perimiter(island, plant)
         result_1 += area * perimiter
-        print(f"{plant=}")
         corners = 0
         for p in boundary_points:
-            # plot_garden(p, plant)
-            c = count_point_corners(p, island, plant)
-            print(f"This point has {c} corners")
-            corners += c
-            print(f"Total {corners=}")
-            # input("----")
+            c = count_point_corners(p, island)
+            corners += count_point_corners(p, island)
         result_2 += area * corners
-        # m = f"{plant=}\n{corners=}"
-        # message.append(m)
 
 
-# for m in message:
-#     print(m)
-LOW = 512704
-HIGH = 986593
-# assert LOW < result_2 < HIGH, f"{result_2} not in range ({LOW}-{HIGH})"
 print("1:", result_1)
 print("2:", result_2)

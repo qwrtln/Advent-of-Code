1a98fd960aa635106545643be53c9abe23516df9
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-15 16:48:20 +0100

    [2024] Day 15 part 2

--- 2024/15.py
+++ 2024/15.py
@@ -1,3 +1,15 @@
+# Benchmark: CPython (3.12.7)
+#  Time (mean ± σ):      93.7 ms ±   5.0 ms    [User: 82.2 ms, System: 11.1 ms]
+#  Range (min … max):    89.1 ms … 106.6 ms    31 runs
+#
+# Benchmark: pypy (3.10.14-7.3.17)
+#   Time (mean ± σ):     202.9 ms ±  13.1 ms    [User: 177.4 ms, System: 25.6 ms]
+#   Range (min … max):   192.5 ms … 241.8 ms    15 runs
+#
+import bisect
+import collections
+import queue
+
 DIRECTIONS = {
     "^": (-1, 0),
     ">": (0, 1),
@@ -11,14 +23,14 @@ def parse_puzzle(file):
     warehouse = []
     robot = (0, 0)
     index = 0
-    for index, line in enumerate(puzzle):
+    for i, line in enumerate(puzzle, start=1):
         if "@" in line:
             robot = (index, line.index("@"))
             warehouse.append(list(line.replace("@", ".")))
         elif line != "":
             warehouse.append(list(line))
         else:
-            index += 1
+            index = i
             break
     movements = []
     for line in puzzle[index:]:
@@ -31,35 +43,139 @@ def handle_movement(robot, movement, warehouse):
     r_y, r_x = robot
     y_n, x_n = r_y + dy, r_x + dx
     if warehouse[y_n][x_n] == ".":
-        return (y_n, x_n), warehouse
+        return (y_n, x_n)
     elif warehouse[y_n][x_n] == "#":
-        return robot, warehouse
+        return robot
     else:
-        potentially_movable = []
+        y_first, x_first = (y_n, x_n)
         while warehouse[y_n][x_n] == "O":
-            potentially_movable.append((y_n, x_n))
             y_n, x_n = y_n + dy, x_n + dx
         if warehouse[y_n][x_n] == "#":
-            return robot, warehouse
-        y_first, x_first = potentially_movable[0]
-        robot = (y_first, x_first)
+            return robot
         warehouse[y_n][x_n] = "O"
         warehouse[y_first][x_first] = "."
-        return robot, warehouse
+        return (y_first, x_first)
 
 
-def plot_warehouse(warehouse, robot, last_movement=None):
-    if last_movement:
-        print("Last movement:", last_movement)
-    result = ""
-    for y in range(len(warehouse)):
-        for x in range(len(warehouse[y])):
-            if (y, x) == robot:
-                result += "@"
-            else:
-                result += warehouse[y][x]
-        result += "\n"
-    print(result)
+def parse_widened_puzzle(file):
+    puzzle = open(file).read().strip().split("\n")
+    warehouse = []
+    index = 0
+    movements = []
+    for y in range(len(puzzle)):
+        line = []
+        if puzzle[y] == "":
+            index = y + 1
+            break
+        for x in range(len(puzzle[y])):
+            if puzzle[y][x] == ".":
+                line.extend([".", "."])
+            elif puzzle[y][x] == "#":
+                line.extend(["#", "#"])
+            elif puzzle[y][x] == "@":
+                line.extend(["@", "."])
+            elif puzzle[y][x] == "O":
+                line.extend(["[", "]"])
+        warehouse.append(line)
+
+    for line in puzzle[index:]:
+        movements.extend(list(line))
+
+    def find_robot(warehouse):
+        for y in range(len(warehouse)):
+            for x in range(len(warehouse[y])):
+                if warehouse[y][x] == "@":
+                    warehouse[y][x] = "."
+                    return (y, x)
+
+    return warehouse, movements, find_robot(warehouse)
+
+
+def get_full_box(y, x, part):
+    if part == "[":
+        return (y, x), (y, x + 1)
+    return (y, x - 1), (y, x)
+
+
+def find_boxes_to_move(y, x, movement, warehouse):
+    box = get_full_box(y, x, warehouse[y][x])
+    boxes = set()
+    q = queue.Queue()
+    q.put(box)
+    while not q.empty():
+        box = q.get()
+        boxes.add(box)
+        new_coords = []
+        if movement in "<>":
+            dy, dx = {"<": (0, -1), ">": (1, 1)}[movement]
+            y_n, x_n = box[dy][0], box[dy][1] + dx
+            if warehouse[y_n][x_n] in "[]":
+                new_coords.append((y_n, x_n))
+        elif movement in "^v":
+            dy = {"^": -1, "v": 1}[movement]
+            for y, x in box:
+                y_n, x_n = y + dy, x
+                if warehouse[y_n][x_n] in "[]":
+                    new_coords.append((y_n, x_n))
+        for y, x in new_coords:
+            q.put(get_full_box(y, x, warehouse[y][x]))
+    return boxes
+
+
+def are_boxes_movable(boxes, movement, warehouse):
+    edge_indices = collections.defaultdict(list)
+    dy, dx, reverse = {
+        "^": (-1, 0, 1),
+        "v": (1, 0, -1),
+        "<": (0, -1, 1),
+        ">": (0, 1, -1),
+    }[movement]
+    if dy != 0:
+        for box in boxes:
+            for y, x in box:
+                bisect.insort(edge_indices[x], y)
+        for x, col in edge_indices.items():
+            for y in col[::reverse]:
+                if y + dy not in col:
+                    if warehouse[y + dy][x] != ".":
+                        return False
+    else:
+        for box in boxes:
+            for y, x in box:
+                bisect.insort(edge_indices[y], x)
+        for y, col in edge_indices.items():
+            for x in col[::reverse]:
+                if x + dx not in col:
+                    if warehouse[y][x + dx] != ".":
+                        return False
+    return True
+
+
+def move_boxes(boxes, movement, warehouse):
+    for box in boxes:
+        for y, x in box:
+            warehouse[y][x] = "."
+    dy, dx = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}[movement]
+    for box in boxes:
+        (y_1, x_1), (y_2, x_2) = box
+        warehouse[y_1 + dy][x_1 + dx] = "["
+        warehouse[y_2 + dy][x_2 + dx] = "]"
+
+
+def handle_widened_movement(robot, movement, warehouse):
+    dy, dx = DIRECTIONS[movement]
+    r_y, r_x = robot
+    y_n, x_n = r_y + dy, r_x + dx
+    if warehouse[y_n][x_n] == ".":
+        return (y_n, x_n)
+    elif warehouse[y_n][x_n] == "#":
+        return robot
+    else:
+        boxes_cluster = find_boxes_to_move(y_n, x_n, movement, warehouse)
+        if are_boxes_movable(boxes_cluster, movement, warehouse):
+            move_boxes(boxes_cluster, movement, warehouse)
+            return (y_n, x_n)
+        return robot
 
 
 def calculate_result(warehouse, box):
@@ -71,15 +187,20 @@ def calculate_result(warehouse, box):
     return result
 
 
-def main():
-    warehouse, movements, robot = parse_puzzle("inputs/15.txt")
-
+def solve_part_1(file):
+    warehouse, movements, robot = parse_puzzle(file)
     for m in movements:
-        robot, warehouse = handle_movement(robot, m, warehouse)
-        # plot_warehouse(warehouse, robot, m)
-    result_1 = calculate_result(warehouse, "O")
-    print("1:", result_1)
+        robot = handle_movement(robot, m, warehouse)
+    return calculate_result(warehouse, "O")
+
+
+def solve_part_2(file):
+    warehouse, movements, robot = parse_widened_puzzle(file)
+    for m in movements:
+        robot = handle_widened_movement(robot, m, warehouse)
+    return calculate_result(warehouse, "[")
 
 
 if __name__ == "__main__":
-    main()
+    print("1:", solve_part_1("inputs/15.txt"))
+    print("2:", solve_part_2("inputs/15.txt"))

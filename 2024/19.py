4bf3530bceb673ec07e7c7a1d2a0cfb5a0445c4e
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-20 01:10:22 +0100

    Simplify day 19

--- 2024/19.py
+++ 2024/19.py
@@ -1,10 +1,10 @@
 # Benchmark: CPython (3.12.7)
-#   Time (mean ± σ):     519.0 ms ±  15.3 ms    [User: 507.2 ms, System: 10.9 ms]
-#   Range (min … max):   492.7 ms … 545.5 ms    10 runs
+#   Time (mean ± σ):     585.8 ms ±  26.4 ms    [User: 571.2 ms, System: 12.0 ms]
+#   Range (min … max):   559.7 ms … 645.5 ms    10 runs
 #
 # Benchmark: pypy (3.10.14-7.3.17)
-#   Time (mean ± σ):     190.6 ms ±  11.4 ms    [User: 164.2 ms, System: 25.7 ms]
-#   Range (min … max):   179.7 ms … 222.4 ms    16 runs
+#   Time (mean ± σ):     161.1 ms ±   7.3 ms    [User: 140.4 ms, System: 20.5 ms]
+#   Range (min … max):   154.8 ms … 181.4 ms    16 runs
 #
 import functools
 
@@ -12,17 +12,6 @@ puzzle = open("inputs/19.txt").read().strip().split("\n")
 
 PATTERNS = set(p for p in puzzle[0].split(", "))
 DESIGNS = puzzle[2:]
-
-
-@functools.cache
-def is_design_possible(design):
-    if design in PATTERNS:
-        return True
-    for i in range(1, len(design)):
-        prefix, rest = design[:i], design[i:]
-        if prefix in PATTERNS and is_design_possible(rest):
-            return True
-    return False
 
 
 @functools.cache
@@ -36,11 +25,6 @@ def ways_design_possible(design):
     return ways
 
 
-result_1 = 0
-result_2 = 0
-for d in DESIGNS:
-    if is_design_possible(d):
-        result_1 += 1
-        result_2 += ways_design_possible(d)
-print("1:", result_1)
-print("2:", result_2)
+ways = [ways_design_possible(d) for d in DESIGNS]
+print("1:", sum(bool(w) for w in ways))
+print("2:", sum(ways))
